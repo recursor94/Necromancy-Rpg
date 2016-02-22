363441cb11548e7f581d162289689c5b06a0b903
Andrew Spano <Andrew Spano>
2016-02-21 20:13:32 -0500

    changed dictionary of npc name strings and conversations in Quest class to simpler list of conversations

--- Assets/scripts/Quest/Quest.cs
+++ Assets/scripts/Quest/Quest.cs
@@ -12,7 +12,7 @@ public abstract class Quest : MonoBehaviour {
 	// Use this for initialization
 
 	private List<QuestObjective> questObjectives; //a list of all quest objectives a given quest has
-	private Dictionary<string, Conversation> conversationDialogues; //A dictionary containing the name of the npc associated with the conversation, and the conversation
+	private List<Conversation> conversations;
 	void Start () {
 	
 	}
