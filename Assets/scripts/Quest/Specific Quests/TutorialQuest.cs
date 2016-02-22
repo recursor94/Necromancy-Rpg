fe2ab962cc156b3249da8f371425459f9a9b8175
Andrew Spano <Andrew Spano>
2016-02-21 21:22:36 -0500

    created Dialogue controller--main purpose right now is to store active conversations

---
+++ Assets/scripts/Quest/Specific Quests/TutorialQuest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialQuest : Quest{
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log (PlayerInformation.PlayerActor.Id);
+		Debug.Log("Tutorial Quest Initialized");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
