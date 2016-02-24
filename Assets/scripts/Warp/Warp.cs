185e6785d4dd48546f30258622ba1d76eb426369
Andrew Spano <Andrew Spano>
2016-02-24 16:09:04 -0500

    Created Warp script to handle warping betweeen zones of a map

---
+++ Assets/scripts/Warp/Warp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+	public Transform warpTarget;
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		other.transform.position = warpTarget.position;
+	}
+
+}
