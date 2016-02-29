b5cd1a91ce7321cadd090a33fb78fa47eddd2b09
Andrew Spano <Andrew Spano>
2016-02-29 16:26:12 -0500

    added Quest Event handler with static methods to control global quest state

---
+++ Assets/scripts/Quest/QuestEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*Contains list of all active quests and ways to send an event to every active quest or quest of a certain type
+*/
+using System.Collections.Generic;
+
+
+public class QuestEventHandler  {
+
+
+	private static List<Quest> ActiveQuests = new List<Quest>();
+
+	public static void AddActiveQuest(Quest q) {
+		ActiveQuests.Add (q);
+	}
+
+	public static void sendQuestEvent(GameEvent e) {
+
+		/*
+		 * sends an event to every quest in the game,
+		 * the appropriate quests will respond to it.
+		 * kind of sloppy but it should work
+		 */
+		foreach(Quest quest in ActiveQuests) {
+			quest.SendEvent (e);
+		}
+	}
+
+}
