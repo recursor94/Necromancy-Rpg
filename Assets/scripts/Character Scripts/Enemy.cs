3c43cf822cc7cce1c084c62c2427a80082d69d80
Andrew Spano <Andrew Spano>
2016-03-01 08:16:00 -0500

    added droppable item list and xp granted to enemy actor class

--- Assets/scripts/Character Scripts/Enemy.cs
+++ Assets/scripts/Character Scripts/Enemy.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class enemy : Actor {
 
+ public class enemy : Actor { 
 
+	private int baseXp; //unmodified xp granted when defeated
+	private  Dictionary<GameItem, int> droppableItems;  //list of items that can be dropped from the enemy. along with corresponding probability of being dropped.
 
 //	protected int levelOffset; //Determines the offset of the enemy's level to the player
 	public enemy(List<CombatAbility> combatMoveSet, string characterName, int level, int healthCap, Gender gender, string id) : 
 	base(combatMoveSet, characterName, level, healthCap, gender, id) {
 		
 	}
+	public int BaseXp {get; set;}
+
+
+	public void AddDroppableItem(GameItem item) {
+		//add droppable item with 100 percent drop rate
+		droppableItems.Add (item, 100);
+	}
+	public void AddDroppableItem(GameItem item, int dropChance) {
+		//Add droppable item with a specified drop chance
+	}
+
+
+	
+
 
 }
