6884f5008982aecc9387fa52c5d924ea0fbe6b6f
Andrew Spano <Andrew Spano>
2016-03-06 17:19:39 -0500

    Added quest variable class to represent the quest variables

---
+++ Assets/Standard Assets/Quest/QuestScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QuestScript {
+
+    protected Quest quest;
+    protected int currentStage;
+    private static Dictionary<string, object> questVars; //structure for representing name of quest variable and value of it.  We don't know how many or what types they're going to hold.
+
+    public QuestScript(Quest quest) {
+        this.quest = quest;
+        currentStage = quest.Stage;
+        questVars = new Dictionary<string, object>();
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   protected void AddQuestVariable(string varName, object value) {
+        /*
+        Add a quest variable along with a value
+        */
+        questVars.Add(varName, value);
+    } 
+
+    protected void UpdateQuestVariable (string varName, object value) {
+
+        questVars[varName] = value;
+
+    }
+}
---
+++ Assets/Standard Assets/Quest/QuestVariable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+/* represents quest variable 
+*/
+public class QuestVariable <T>  {
+    private string varName;
+    private T value;
+
+
+
+    public QuestVariable(string varName, T value) {
+        this.varName = varName;
+        this.value = value;
+    }
+    public T Value {
+        get {
+            return value;
+        }
+
+        set {
+            this.value = value;
+        }
+    }
+}
