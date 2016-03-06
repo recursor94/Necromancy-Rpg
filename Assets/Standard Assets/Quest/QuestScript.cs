b1eec1e3dcc6ba1811d5928e8858b7ab15252982
Andrew Spano <Andrew Spano>
2016-03-06 18:38:31 -0500

    Added helper methods for getting QuestVariables

--- Assets/Standard Assets/Quest/QuestScript.cs
+++ Assets/Standard Assets/Quest/QuestScript.cs
@@ -6,32 +6,74 @@ public class QuestScript {
 
     protected Quest quest;
     protected int currentStage;
-    private static Dictionary<string, object> questVars; //structure for representing name of quest variable and value of it.  We don't know how many or what types they're going to hold.
+    private static List<QuestVariable<object>> questVars; //structure for representing name of quest variable and value of it.  We don't know how many or what types they're going to hold.
 
     public QuestScript(Quest quest) {
         this.quest = quest;
         currentStage = quest.Stage;
-        questVars = new Dictionary<string, object>();
+        questVars = new List<QuestVariable<object>>();
     }
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-   protected void AddQuestVariable(string varName, object value) {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    protected static void AddQuestVariable(string varName, object value) {
         /*
         Add a quest variable along with a value
         */
-        questVars.Add(varName, value);
-    } 
+        QuestVariable<object> variable = new QuestVariable<object>(varName, value);
+        questVars.Add(variable);
+    }
 
-    protected void UpdateQuestVariable (string varName, object value) {
+    protected void UpdateQuestVariable(QuestVariable<object> variable, object value) {
 
-        questVars[varName] = value;
+        questVars[questVars.IndexOf(variable)].Value = value;
 
+    }
+
+    public static bool TryGetQuestVariableValue(string varName, out int result) {
+        foreach(QuestVariable<object> variable in questVars) {
+            if (variable.VarName.Equals(varName)) {
+                result = (int)variable.Value; //not safe if wrong type, fix later
+                return true;
+            }
+        }
+        result = default(int);
+        return false;  
+    }
+    public static bool TryGetQuestVariableValue(string varName, out string result) {
+        foreach(QuestVariable<object> variable in questVars) {
+            if (variable.VarName.Equals(varName)) {
+                result = (string)variable.Value; //not safe if wrong type, fix later
+                return true;
+            }
+        }
+        result = default(string);
+        return false;  
+    }
+    public static bool TryGetQuestVariableValue(string varName, out bool result) {
+        foreach(QuestVariable<object> variable in questVars) {
+            if (variable.VarName.Equals(varName)) {
+                result = (bool)variable.Value; //not safe if wrong type, fix later
+                return true;
+            }
+        }
+        result = default(bool);
+        return false;  
+    }
+    public static bool TryGetQuestVariableValue(string varName, out double result) {
+        foreach(QuestVariable<object> variable in questVars) {
+            if (variable.VarName.Equals(varName)) {
+                result = (double)variable.Value; //not safe if wrong type, fix later
+                return true;
+            }
+        }
+        result = default(double);
+        return false;  
     }
 }
--- Assets/Standard Assets/Quest/QuestVariable.cs
+++ Assets/Standard Assets/Quest/QuestVariable.cs
@@ -21,4 +21,8 @@ public class QuestVariable <T>  {
             this.value = value;
         }
     }
+
+    public string VarName {
+        get { return varName; }
+    }
 }
