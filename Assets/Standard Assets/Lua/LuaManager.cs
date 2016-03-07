db25c819c0c55a80f45856f2b2659a87d46b3bdb
Andrew Spano <Andrew Spano>
2016-03-07 18:31:42 -0500

    Renamed lua private fields to have underscores, added Lua state variable to class and private constructor

--- Assets/Standard Assets/Lua/LuaManager.cs
+++ Assets/Standard Assets/Lua/LuaManager.cs
@@ -1,25 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using NLua;
 
 
 /* Follows the singleton pattern to 
 *   create one single globally accesed lua environment
 */
 public sealed class LuaManager  {
-    private static volatile LuaManager instance; //thread safe and statically initialized
-    private static object syncRoot = new object(); //lock to make thread safe
+    private static volatile LuaManager _instance; //thread safe and statically initialized
+    private static object _syncRoot = new object(); //lock to make thread safe
+
+    //Now for the instance fields
+    private Lua _LuaEnvironment; //Actual environment/interpreter for game.  I only want one for the game, so the state of all variables can be accessed by any lua script
 	
-    private LuaManager() { } //Empty default contructor to prevent initialization outside of static
+    private LuaManager() {
+        this._LuaEnvironment = new Lua();
+    } //Empty default contructor to prevent initialization outside of static
 
     public static LuaManager Instance {
         get {
-            if(instance == null) {
-                lock(syncRoot) {
-                    if (instance == null)
-                        instance = new LuaManager();
+            if(_instance == null) {
+                lock(_syncRoot) {
+                    if (_instance == null)
+                        _instance = new LuaManager();
                 }
             }
-            return instance;
+            return _instance;
         }
     }
+    //now for the instance methods.  These may not be thread safe and shuld be modified later.
 }
