e15f6957c6c85156d4c7788a5a9c7cb9c33dd05a
Andrew Spano <Andrew Spano>
2016-03-06 22:53:34 -0500

    Added lua test script

---
+++ Assets/Standard Assets/TestLua.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using NLua;
+
+public class TestLua : MonoBehaviour {
+    Lua luaState; 
+	// Use this for initialization
+	void Start () {
+        luaState = new Lua();
+        luaState["x"] = 3;
+
+        Debug.Log(luaState["x"]);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
