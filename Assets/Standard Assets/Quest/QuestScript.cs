c41f992400d11d9bfb289399aaf2b92488214170
Andrew Spano <Andrew Spano>
2016-03-06 22:13:44 -0500

    Added lua interpreter, moved it, and then

--- Assets/Standard Assets/Quest/QuestScript.cs
+++ Assets/Standard Assets/Quest/QuestScript.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using NLua;
 
 public class QuestScript {
 
     protected Quest quest;
     protected int currentStage;
     private static List<QuestVariable<object>> questVars; //structure for representing name of quest variable and value of it.  We don't know how many or what types they're going to hold.
-
     public QuestScript(Quest quest) {
         this.quest = quest;
         currentStage = quest.Stage;
