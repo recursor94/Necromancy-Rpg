0dd767e5bd9b5402f85a2fb8866386dc16aba75b
Andrew Spano <Andrew Spano>
2016-03-07 16:49:48 -0500

    Created static initialization methods for Lua manager class

--- Assets/Standard Assets/Lua/LuaManager.cs
+++ Assets/Standard Assets/Lua/LuaManager.cs
@@ -1,7 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LuaManager  {
 
+/* Follows the singleton pattern to 
+*   create one single globally accesed lua environment
+*/
+public sealed class LuaManager  {
+    private static volatile LuaManager instance; //thread safe and statically initialized
+    private static object syncRoot = new object(); //lock to make thread safe
 	
+    private LuaManager() { } //Empty default contructor to prevent initialization outside of static
+
+    public static LuaManager Instance {
+        get {
+            if(instance == null) {
+                lock(syncRoot) {
+                    if (instance == null)
+                        instance = new LuaManager();
+                }
+            }
+            return instance;
+        }
+    }
 }
