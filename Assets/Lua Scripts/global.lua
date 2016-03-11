788e3c0e68f49a19e7fa7d99addf1cf927b5cca7
Andrew Spano <Andrew Spano>
2016-03-11 06:06:46 -0500

    Added Game script and quest script prototypes

--- Assets/Lua Scripts/global.lua
+++ Assets/Lua Scripts/global.lua
@@ -15,39 +15,93 @@ function teleport(movingtransform, destinationtransform) --Takes two transforms 
 end
 
 --Define prototype for game scripts:
-GameScript = {}
-function GameScript:new(o)
-	local o = o or {} 
-	setmetatable(o, self)
-	self.__index = self
-	return o
-end
-function GameScript:onStart()
-	Debug.Log("Starting")
-end
-function GameScript:onUpdate()
-	Debug.Log("Updating")
-end
---End game script prototype
+--GameScript = {}
+--function GameScript:new(o)
+--	local o = o or {} 
+--	setmetatable(o, self)
+--	self.__index = self
+--	return o
+--end
+--function GameScript:onStart()
+--	Debug.Log("Starting")
+--end
+--function GameScript:onUpdate()
+--	Debug.Log("Updating")
+--end
+----End game script prototype
+----
+----Quest script prototype:
+--Debug.Log(GameScript)
 --
---Quest script prototype:
-Debug.Log(GameScript)
-
-QuestScript = GameScript:new({quest})
-Debug.Log(QuestScript)
-
-function QuestScript:new(quest)
-	self.quest = quest --argument passed must be C# Quest instance
-	self.currentStage = quest.Stage
-	return self
+--QuestScript = GameScript:new({quest})
+--Debug.Log(QuestScript)
+--
+--function QuestScript:new(quest)
+--	self.quest = quest --argument passed must be C# Quest instance
+--	self.currentStage = quest.Stage
+--	return self
+--end
+--function QuestScript:update()
+--	if self.currentStage == quest.Stage then
+--		currentStage = quest.Stage
+--		onStageChange(quest.Stage)
+--	end
+--end
+--
+--function QuestScript:onStageChange(stage)
+--end
+--
+--Prototype for Game Script:
+GameScript = {}
+GameScript.__index = GameScript
+--Constructor:
+function GameScript.new()
+	local instance = {}
+	--set Gamescript as prototype for new instance:
+	setmetatable(instance, GameScript)
+	return instance
 end
-function QuestScript:update()
-	if self.currentStage == quest.Stage then
-		currentStage = quest.Stage
-		onStageChange(quest.Stage)
-	end
+
+--method definitions:
+function GameScript:onStart()
+	Debug.Log("Starting lua script")
 end
 
+function GameScript:onUpdate()
+	Debug.Log("Lua script finished")
+end
+
+function GameScript:onFinish()
+	Debug.Log("Lua script finished")
+end
+
+
+--Quest Script class
+QuestScript = {}
+--constructor
+QuestScript.__index = QuestScript
+
+function QuestScript.new(quest)
+	local instance = {
+		_quest = quest,
+		_currentStage = quest.Stage
+	}
+
+	setmetatable(instance, QuestScript)
+	return instance
+end
+--make GameScript prototype for QuestScript
+setmetatable(QuestScript, {__index = GameScript})
+
+--instance methods
 function QuestScript:onStageChange(stage)
+	Debug.Log("Stage change detected, new stage is " .. stage)
+end
+function QuestScript:onUpdate()
+	if not self._currentStage == self.quest.Stage then
+		self._currentStage = quest.Stage
+		self.OnstageChange(self.quest.stage)
+	end
+	GameScript:onUpdate(self)
 end
 
