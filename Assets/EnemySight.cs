f36ed5b5d7fc9a6601c8e7aa35b1c0205fb62519
Andrew Spano <Andrew Spano>
2016-03-16 21:01:54 -0400

    Added enemy field of view script

---
+++ Assets/EnemySight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour {
+    public float fieldOfViewAngle = 110f; //field of view of enemy
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
